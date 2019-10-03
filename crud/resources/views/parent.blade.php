d6b8adafdf138766be846392cf7ddfb0f16a4f51
Nemanja2016 <Nemanja2016@nemanja>
2019-10-04 01:16:03 +0200

    crud laravel tutorial

---
+++ crud/resources/views/parent.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<h1>CRUD</h1>
+		@yield('main')
+	</div>
+
+</body>
+</html>
